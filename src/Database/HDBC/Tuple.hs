e4b1d9c7c40ca4f16301ed18f823fbc6930fb780
Kei Hibino <ex8k.hibino@gmail.com>
2013-03-05 18:21:03 +0900

    Renamed.

--- src/Database/HDBC/Tuple.hs
+++ src/Database/HDBC/Tuple.hs
@@ -1,43 +1,42 @@
 {-# LANGUAGE FlexibleContexts #-}
 
 module Database.HDBC.Tuple (
-  RecordFromSql, recordFromSql, takeRecord', width, toRecord',
+  RecordFromSql, createRecordFromSql,
+  takeRecord', width, runToRecord,
 
   singleton, (<&>),
 
-  outer
-  ) where
+  outer,
+) where
 
 import Data.Convertible (Convertible)
 import Database.HDBC (SqlValue(SqlNull), fromSql)
 
 data RecordFromSql a =
   RecordFromSql
-  { toRecord' :: [SqlValue] -> a
+  { runToRecord :: [SqlValue] -> a
   , width     :: !Int
   }
 
-recordFromSql :: ([SqlValue] -> a) -> Int -> RecordFromSql a
-recordFromSql =  RecordFromSql
+createRecordFromSql :: ([SqlValue] -> a) -> Int -> RecordFromSql a
+createRecordFromSql =  RecordFromSql
 
 takeRecord' :: RecordFromSql a -> [SqlValue] -> (a, [SqlValue])
-takeRecord' rec vals = (toRecord' rec va, vr) where
+takeRecord' rec vals = (runToRecord rec va, vr) where
   (va, vr) = splitAt (width rec) vals
 
 singleton :: Convertible SqlValue a => RecordFromSql a
-singleton =  RecordFromSql { toRecord' = fromSql . head, width = 1 }
+singleton =  RecordFromSql { runToRecord = fromSql . head, width = 1 }
 
 (<&>) :: RecordFromSql a -> RecordFromSql b -> RecordFromSql (a, b)
-a <&> b = RecordFromSql { toRecord' = toR, width = width a + width b } where
+a <&> b = RecordFromSql { runToRecord = toR, width = width a + width b } where
   toR vals = let (ra, vals') = takeRecord' a vals
              in  (ra, fst $ takeRecord' b vals')
 
 infixl 4 <&>
 
 outer :: RecordFromSql a -> Int -> RecordFromSql (Maybe a)
-outer rec pkeyIdx = RecordFromSql { toRecord' = mayToRec, width = width rec } where
+outer rec pkeyIdx = RecordFromSql { runToRecord = mayToRec, width = width rec } where
   mayToRec vals | vals !! pkeyIdx /= SqlNull = Just . fst $ takeRecord' rec vals
                 | otherwise                  = Nothing
-
-
 
