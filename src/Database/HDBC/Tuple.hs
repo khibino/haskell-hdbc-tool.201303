0027a346261b6af813646771a026b679de46d301
Kei Hibino <ex8k.hibino@gmail.com>
2013-03-05 18:17:08 +0900

    Renamed.

--- src/Database/HDBC/Tuple.hs
+++ src/Database/HDBC/Tuple.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 
 module Database.HDBC.Tuple (
-  RecordFromSql, takeRecord, width, toRecord',
+  RecordFromSql, recordFromSql, takeRecord', width, toRecord',
 
   singleton, (<&>),
 
@@ -17,8 +17,11 @@ data RecordFromSql a =
   , width     :: !Int
   }
 
-takeRecord :: RecordFromSql a -> [SqlValue] -> (a, [SqlValue])
-takeRecord rec vals = (toRecord' rec va, vr) where
+recordFromSql :: ([SqlValue] -> a) -> Int -> RecordFromSql a
+recordFromSql =  RecordFromSql
+
+takeRecord' :: RecordFromSql a -> [SqlValue] -> (a, [SqlValue])
+takeRecord' rec vals = (toRecord' rec va, vr) where
   (va, vr) = splitAt (width rec) vals
 
 singleton :: Convertible SqlValue a => RecordFromSql a
@@ -26,12 +29,15 @@ singleton =  RecordFromSql { toRecord' = fromSql . head, width = 1 }
 
 (<&>) :: RecordFromSql a -> RecordFromSql b -> RecordFromSql (a, b)
 a <&> b = RecordFromSql { toRecord' = toR, width = width a + width b } where
-  toR vals = let (ra, vals') = takeRecord a vals
-             in  (ra, fst $ takeRecord b vals')
+  toR vals = let (ra, vals') = takeRecord' a vals
+             in  (ra, fst $ takeRecord' b vals')
 
 infixl 4 <&>
 
 outer :: RecordFromSql a -> Int -> RecordFromSql (Maybe a)
 outer rec pkeyIdx = RecordFromSql { toRecord' = mayToRec, width = width rec } where
-  mayToRec vals | vals !! pkeyIdx /= SqlNull = Just . fst $ takeRecord rec vals
+  mayToRec vals | vals !! pkeyIdx /= SqlNull = Just . fst $ takeRecord' rec vals
                 | otherwise                  = Nothing
+
+
+
