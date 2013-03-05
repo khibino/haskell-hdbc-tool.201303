45258474b6cd08c245f536febbdcd900bfb3cee4
Kei Hibino <ex8k.hibino@gmail.com>
2013-03-05 19:12:17 +0900

    Define type class to use type inference information.

--- src/Database/HDBC/Tuple.hs
+++ src/Database/HDBC/Tuple.hs
@@ -7,6 +7,11 @@ module Database.HDBC.Tuple (
   singleton, (<&>),
 
   outer,
+
+  PrimaryKey, createPrimaryKey, index,
+
+  FromSql (recordFromSql),
+  takeRecord,
 ) where
 
 import Data.Convertible (Convertible)
@@ -35,8 +40,32 @@ a <&> b = RecordFromSql { runToRecord = toR, width = width a + width b } where
 
 infixl 4 <&>
 
-outer :: RecordFromSql a -> Int -> RecordFromSql (Maybe a)
-outer rec pkeyIdx = RecordFromSql { runToRecord = mayToRec, width = width rec } where
-  mayToRec vals | vals !! pkeyIdx /= SqlNull = Just . fst $ takeRecord' rec vals
+newtype PrimaryKey a = PrimaryKey { index :: Int }
+
+createPrimaryKey :: Int -> PrimaryKey a
+createPrimaryKey = PrimaryKey
+
+outer :: RecordFromSql a -> PrimaryKey a -> RecordFromSql (Maybe a)
+outer rec pkey = RecordFromSql { runToRecord = mayToRec, width = width rec } where
+  mayToRec vals | vals !! index pkey /= SqlNull = Just . fst $ takeRecord' rec vals
                 | otherwise                  = Nothing
 
+
+class FromSql a where
+  recordFromSql :: RecordFromSql a
+
+instance (FromSql a, FromSql b) => FromSql (a, b) where
+  recordFromSql = recordFromSql <&> recordFromSql
+
+
+-- newtype PrimaryKey a = PrimaryKey { index :: Int }
+
+class HasPrimaryKey a where
+  primaryKey :: PrimaryKey a
+
+instance (HasPrimaryKey a, FromSql a) => FromSql (Maybe a) where
+  recordFromSql = outer recordFromSql $ primaryKey
+
+
+takeRecord :: FromSql a => [SqlValue] -> (a, [SqlValue])
+takeRecord =  takeRecord' recordFromSql
